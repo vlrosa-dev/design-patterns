98867cce9cc2a5a793683d1143682355552b3604
Ricardo Mendes <rmcs87@gmail.com>
2020-09-23 22:07:09 -0300

    Builder - Interface

---
+++ 11_Builder/IEmailBuilder.cs
@@ -0,0 +1,9 @@
+﻿public interface IEmailBuilder
+{
+    IEmailBuilder Body(string content);
+    IEmailBuilder From(string address);
+    IEmailBuilder To(string address);
+    IEmailBuilder Subject(string address);
+    IEmailBuilder Anexo(string fileName);
+    Email Build();
+}
