957827151ebf2148512852a14c98acb42cad0990
Ricardo Mendes <rmcs87@gmail.com>
2020-10-01 20:22:09 -0300

    Adapter - base

---
+++ 14_Adapter/HRSystem.cs
@@ -0,0 +1,14 @@
+﻿public class HRSystem
+{
+    public string[][] GetEmpyees()
+    {
+        string[][] employees = new string[4][];
+        employees[0] = new string[] { "15", "Ricardo", "Professor" };
+        employees[1] = new string[] { "1", "Susi", "Corrdenadora" };
+        employees[2] = new string[] { "17", "Saulo", "Professor" };
+        employees[3] = new string[] { "22", "Jean", "Professor" };
+
+        return employees;
+    }
+}
+
