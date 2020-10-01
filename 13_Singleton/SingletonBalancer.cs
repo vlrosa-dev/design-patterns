1ce5f702a75be68ea2928870e81d0023a600e8d9
Ricardo Mendes <rmcs87@gmail.com>
2020-09-30 22:15:57 -0300

    Singleton - singleton

---
+++ 13_Singleton/SingletonBalancer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class SingletonBalancer
+{
+    private static SingletonBalancer _instance;
+	private List<string> _freeServers = new List<string>();
+	private List<string> _busyServers = new List<string>();
+	private Random _random = new Random();
+	private SingletonBalancer()
+	{
+		_freeServers.Add("server1");
+		_freeServers.Add("server2");
+		_freeServers.Add("server3");
+		_freeServers.Add("server4");
+		_freeServers.Add("server5");
+		_freeServers.Add("server6");
+	}
+	public static SingletonBalancer GetInstance()
+	{
+		if (_instance == null)
+		{
+			_instance = new SingletonBalancer();
+		}
+		return _instance;
+	}
+	public string GetServer()
+	{
+		if (_freeServers.Count < 1)
+			throw new Exception("No more Servers");
+
+		int r = _random.Next(_freeServers.Count);
+		string server = _freeServers[r];
+		_busyServers.Add(server);
+		_freeServers.Remove(server);
+		return server;
+	}
+}
+
